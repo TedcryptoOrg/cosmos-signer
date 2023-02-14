fad141e1795771bc2fecd359efdbb904103dba15
Jose Lopes <josluis.lopes@gmail.com>
2023-02-14 11:17:37 +0000

    Expose more classes and functionalities

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
+import { Amino } from './Amino';
+import { Registry } from './Registry';
 import { SigningClient } from './SigningClient';
+import { AminoMessageCollator } from './types/AminoMessageCollator';
+import { Message } from "./types/Message";
+import { MessageCollator } from "./types/MessageCollator";
+import { Network } from "./types/Network";
+import { coin } from './util/Coin';
+import { parseTxResult } from './util/TransactionHelper';
 
-export {SigningClient};
+export {
+    // classes
+    SigningClient,
+    Amino,
+    Registry,
+    // types
+    AminoMessageCollator,
+    Message,
+    MessageCollator,
+    Network,
+    // functions
+    coin,
+    parseTxResult
+};
