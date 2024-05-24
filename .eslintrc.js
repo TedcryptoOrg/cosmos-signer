29b90bfe1ec82df75365be6b56bb2f18106b633d
Jose Lopes <josluis.lopes@gmail.com>
2024-05-24 15:04:45 +0100

    chore: prepare new linter

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    overrides: [
+        {
+            files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+            extends: 'love',
+            "rules": {
+                "@typescript-eslint/no-var-requires": "off"
+            }
+        }
+    ],
+    ignorePatterns: [
+        'jest.config.ts',
+        '.eslintrc.js',
+        'babel.config.js',
+        'node_modules/',
+        'tests/',
+        'dist/'
+    ],
+}
