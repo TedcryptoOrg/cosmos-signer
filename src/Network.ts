5529da77f978b9e067432a5ea4b62fbb91b3d4f2
Luis Lopes <josluis.lopes@gmail.com>
2024-05-24 15:56:09 +0100

    chore: add es-lint-config-love (#191)
    * chore: prepare new linter
    
    * chore: fix all lint issues

--- src/Network.ts
+++ src/Network.ts
@@ -1,19 +1,18 @@
-import { type Network as NetworkData } from "./types/Network";
-import {Chain, CosmosDirectory} from "@tedcryptoorg/cosmos-directory";
+import { type Network as NetworkData } from './types/Network'
+import { type Chain, CosmosDirectory } from '@tedcryptoorg/cosmos-directory'
 
 export class Network {
-    constructor(public readonly data: NetworkData) {}
+  constructor (public readonly data: NetworkData) {}
 
-    public static createFromChain(chain: Chain, txTimeout: number = 1000, restUrl: string|undefined = undefined): Network
-    {
-        return new Network({
-            chain_name: chain.name,
-            authzAminoSupport: chain.params.authz ?? false,
-            prefix: chain.bech32_prefix,
-            txTimeout: txTimeout,
-            coinType: chain.slip44,
-            chainId: chain.chain_id,
-            restUrl: restUrl ?? new CosmosDirectory().restUrl(chain.name)
-        })
-    }
+  public static createFromChain (chain: Chain, txTimeout: number = 1000, restUrl: string | undefined = undefined): Network {
+    return new Network({
+      chain_name: chain.name,
+      authzAminoSupport: chain.params.authz ?? false,
+      prefix: chain.bech32_prefix,
+      txTimeout,
+      coinType: chain.slip44,
+      chainId: chain.chain_id,
+      restUrl: restUrl ?? new CosmosDirectory().restUrl(chain.name)
+    })
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import { Network } from './Network';
-import { Registry } from './Registry';
-import { EthSigner } from './Signers/EthSigner';
-import { Signer } from './Signers/Signer';
-import { SigningClient } from './SigningClient';
-import { AminoMessageCollator } from './types/AminoMessageCollator';
-import { Message } from "./types/Message";
-import { MessageCollator } from "./types/MessageCollator";
-import { type Network as NetworkData } from "./types/Network";
-import { coin } from './util/Coin';
-import { parseTxResult } from './util/TransactionHelper';
+import { Network } from './Network'
+import { Registry } from './Registry'
+import { EthSigner } from './Signers/EthSigner'
+import { Signer } from './Signers/Signer'
+import { SigningClient } from './SigningClient'
+import { type AminoMessageCollator } from './types/AminoMessageCollator'
+import { type Message } from './types/Message'
+import { type MessageCollator } from './types/MessageCollator'
+import { type Network as NetworkData } from './types/Network'
+import { coin } from './util/Coin'
+import { parseTxResult } from './util/TransactionHelper'
 
 export {
-    // classes
-    SigningClient,
-    Registry,
-    EthSigner,
-    Signer,
-    Network,
-    // types
-    AminoMessageCollator,
-    Message,
-    MessageCollator,
-    NetworkData,
-    // functions
-    coin,
-    parseTxResult
-};
+  // classes
+  SigningClient,
+  Registry,
+  EthSigner,
+  Signer,
+  Network,
+  // types
+  type AminoMessageCollator,
+  type Message,
+  type MessageCollator,
+  type NetworkData,
+  // functions
+  coin,
+  parseTxResult
+}
